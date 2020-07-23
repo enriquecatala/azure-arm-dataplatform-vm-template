0478296b6b4c2c689d3cc887fe2506a9a0ba603f
Enrique Catala Bañuls <enrique@enriquecatala.com>
2020-07-23 16:36:33 +0200

    added windows terminal

--- CustomScripts/installsoftware.ps1
+++ CustomScripts/installsoftware.ps1
@@ -15,7 +15,8 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
-$Packages = 'googlechrome',`
+$Packages = 'microsoft-windows-terminal',`
+            'googlechrome',`
             'git',`
             'visualstudiocode',`            
             'azure-data-studio',`
